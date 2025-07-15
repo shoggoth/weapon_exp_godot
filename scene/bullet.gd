3bff7e3bb357eb0e141733f99be30407b32903ef
rich@wight <r.j.henry@gmail.com>
2025-07-15 10:45:09 +0100

    Added an experimental bullet

---
+++ scene/bullet.gd
@@ -0,0 +1,21 @@
+extends HitBox
+
+@export var speed_multiplier: float = 1.0
+@export var duration: float = 1.0
+
+var direction: Vector2
+
+func _ready():
+	go(direction)
+	
+func go(direction: Vector2) -> bool:
+	var move_tween = create_tween()
+	move_tween.tween_property(self, "position", direction * speed_multiplier, duration).as_relative()
+	move_tween.tween_callback(func(): queue_free())
+	if direction.y: rotation = PI * 0.5
+	$Fire.play()
+	return true
+	
+func _on_body_entered(body):
+	if !(body is TileMapLayer): push_error("Bullet detected spurious body collision")
+	queue_free()
--- scene/player.gd
+++ scene/player.gd
@@ -1,5 +1,4 @@
 class_name Player extends CharacterBody2D
 
-
 func _on_weapon_component_weapon_fired(weapon: Weapon) -> void:
 	print("%s fired %s" % [weapon, weapon.name])

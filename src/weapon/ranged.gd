194ae0baf2546921efaa83386b81343a3e82994e
rich@wight <r.j.henry@gmail.com>
2025-07-17 16:46:17 +0100

    Pistol is firing

--- src/weapon/ranged.gd
+++ src/weapon/ranged.gd
@@ -1,3 +1,7 @@
 class_name RangedWeapon extends Weapon
 
 @export var bullet: PackedScene
+
+func fire(from_node: Node2D, direction: Vector2 = Vector2.ZERO, completion: Callable = Global.null_callable) -> bool:
+	from_node.add_child(bullet.instantiate())
+	return true
